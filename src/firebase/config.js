5d9d38f969e6b425a8ed36c0fb33a035a3cdc00a
Gayrat <gena68@inbox.ru>
2019-12-05 22:12:44 +0300

    config положил

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: "vlasov-react-gloacademy-diplom.firebaseapp.com",
+    databaseURL: "https://vlasov-react-gloacademy-diplom.firebaseio.com",
+    projectId: "vlasov-react-gloacademy-diplom",
+    storageBucket: "vlasov-react-gloacademy-diplom.appspot.com",
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
+  };
+
+
+
+
+  export default firebaseConfig;
